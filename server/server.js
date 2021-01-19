f4c58c7859b13022f93aeb2fd4be86fe456924ca
nowled <nowled@excite.com>
2021-01-19 07:15:08 +0300

    Working On Errors

--- server/server.js
+++ server/server.js
@@ -1,24 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 const app = express();
 
 dotenv.config();
 
+connectDB();
+
 app.get('/', (req, res) => {
   res.send('Api is working');
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+
+app.use('/api/products', productRoutes);
+
+app.use((err, req, res, next) => {
+  const error = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
